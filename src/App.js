5598388307e439e092c14e4c1a54b32bb57c813f
Christopher Foley <chrisninetyone@gmail.com>
2020-10-03 15:21:29 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Starting my portfilio project. Woohoo!
         </p>
         <a
           className="App-link"
