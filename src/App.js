670e3a42e8e342e30e7bd1cb6b1709802731eefb
Christopher Foley <chrisninetyone@gmail.com>
2020-11-08 21:27:41 -0500

    Added React Router and implemented routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { Route, Switch, Redirect } from "react-router-dom";
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import About from './components/About';
+// import About from './components/About';
+// import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Home/>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        {/* <Route path="/gallery" component={Gallery} />
+        <Route path="/contact" component={Contact} /> */}
+        {/* <Route path="/sample" render={routerProps => <Sample {...routerProps} sampleProp={"sample"}/>} />
+        <Route path="/something" component={Something} />
+        <Route path='/default' render={() => <Redirect to= "/" />} />
+        <Route component={NoMatch} /> */}
+      </Switch>
+      {/* <Home/> */}
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,13 @@ import './Navbar.css'
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <a className="logo" href="">
+            <a className="logo" href="/">
                 <img src={paintbrush} alt=""/>
             </a>
             <ul className="navbar-ul">
-                <li><a href="#" className="navbar-link">About</a></li>
-                <li><a href="#" className="navbar-link">Gallery</a></li>
-                <li><a href="#" className="navbar-link">Contact</a></li>
+                <li><a href="/about" className="navbar-link">About</a></li>
+                <li><a href="/gallery" className="navbar-link">Gallery</a></li>
+                <li><a href="/contact" className="navbar-link">Contact</a></li>
             </ul>
         </nav>
     );
